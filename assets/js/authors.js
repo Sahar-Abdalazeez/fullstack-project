e434513239428f495b95ec2daeff0b73c88c4f16
sahar samer <sahar@aui.io>
2022-06-30 00:32:13 +0300

    add : sahar code

---
+++ assets/js/authors.js
@@ -0,0 +1,188 @@
+let title = document.querySelector('#title');
+let authorName = document.querySelector('#name');
+let authorInfo = document.querySelector('#authorInfo');
+let addBtn = document.querySelector(".add_btn");
+let inputFileSelector = document.querySelector('.input-file-selector');
+let data =document.querySelector("#data");
+let updateBtn = document.querySelectorAll("#updateBtn");
+let theTeamDiv= document.querySelector(".theTeam");
+let table_title = document.querySelectorAll(".table_title");
+let table_name = document.querySelectorAll(".table_name");
+let table_info = document.querySelectorAll(".table_info");
+let clearAll = document.querySelector(".clr-btn");
+let teamM =Array.from(document.querySelectorAll(".teamM"));
+let img;
+let imgFiles = document.querySelector(".img-file");
+let tabelIndex;
+let authorsIndex;
+let author;
+let servicesImg = document.querySelectorAll(".services-img");
+let inputs = document.querySelectorAll(".inputs");
+let authors=[
+    author ={
+    
+    image: "assets/img/img1.jpg",
+    Title:"author",
+    Name:"Sahar Abdul Aziz",
+    AuthorInfo:'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Quia, Eum.'
+},
+author ={
+   
+    image: "assets/img/img2.jpg",
+    Title:"designer",
+    Name:"ayman",
+    AuthorInfo:'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Quia, Eum.'
+},
+author ={
+    
+    image: "assets/img/img3.jpg",
+    Title:"consultant",
+    Name:"mohammad",
+    AuthorInfo:'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Quia, Eum.'
+},
+author ={
+    
+    image: "assets/img/img4.jpg",
+    Title:"support",
+    Name:"momen",
+    AuthorInfo:'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Quia, Eum.'
+}
+];
+
+console.log('data', data);
+
+inputFileSelector.onclick=()=>{
+    imgFiles.click();
+}
+// displayData();
+// displayDataInTable();
+
+if(localStorage.getItem("authors")){
+    authors=JSON.parse(localStorage.getItem("authors"));
+    displayDataInTable();
+    displayData();
+}
+
+
+addBtn.addEventListener("click",()=>{
+    if(addBtn.innerHTML=="update Author"){
+        updateRow();
+        displayDataInTable();
+    }else{
+        addAuthor();
+        displayDataInTable();
+        displayData();
+        clearInputs();
+    }
+    
+        
+    localStorage.setItem("authors",JSON.stringify(authors));
+})
+
+
+
+imgFiles.addEventListener("change", function(){
+    const reader = new FileReader();
+    reader.addEventListener('load', ()=>{
+        img = reader.result;
+    }) 
+    reader.readAsDataURL(this.files[0]);
+});
+
+function addAuthor(){
+     author ={
+        image:img,
+        Title:title.value,
+        Name:authorName.value,
+        AuthorInfo:authorInfo.value
+    }
+    authors.push(author);
+    console.log('authors',authors);
+    displayDataInTable();
+}
+
+function displayDataInTable(){
+    let result='';
+    for(let i=0;i<authors.length;i++){
+        result+=`
+        <tr>
+            <td>${i}</td>
+            <td>${authors[i].Title}</td>
+            <td>${authors[i].Name}</td>
+            <td>${authors[i].AuthorInfo}</td>
+            <td><img class="table-image" src="${authors[i].image}"></td>
+            <td><button class="btn btn-outline-info"  onclick="getAuthorsData(${i})"> Update </button></td>
+            <td><button class="deleteBtn btn btn-outline-danger" onclick="deleteTableAuthor(${i})"> Delete </button></td>
+        </tr>
+        `
+    }
+    data.innerHTML=result;
+}
+function getAuthorsData(i){
+    addBtn.innerHTML="update Author";
+    title.value=authors[i].Title;
+    authorName.value=authors[i].Name;
+    authorInfo.value=authors[i].AuthorInfo;
+    tabelIndex=i;
+}
+
+function updateRow(){
+    if(img==undefined){
+        authors[tabelIndex].Title=title.value;
+        authors[tabelIndex].Name=authorName.value;
+        authors[tabelIndex].AuthorInfo=authorInfo.value;
+    }else{
+        authors[tabelIndex].Title=title.value;
+        authors[tabelIndex].Name=authorName.value;
+        authors[tabelIndex].AuthorInfo=authorInfo.value;
+        authors[tabelIndex].image=img;
+    }
+
+    displayData();
+    clearInputs();
+    addBtn.innerHTML="Add author";
+}
+
+function clearInputs(){
+     for(let i=0;i<inputs.length;i++){
+        inputs[i].value="";
+    }
+}
+
+
+function displayData(){
+    let result='';
+    for(let i =0; i <authors.length;i++){
+        result+=`
+        <div class="teamM text-center  w-25">
+            <img src="${authors[i].image}" alt="${authors[i].Name}" class="services-img" >
+            <p class="table_title text-center text-uppercase lightGrey mt-4">${authors[i].Title}</p>
+            <h3 class="table_name text-capitalize">${authors[i].Name}</h3>
+            <p class="table_info text-capitalize px-5 mx-5">${authors[i].AuthorInfo}</p>
+        </div>
+        `
+    }
+    theTeamDiv.innerHTML=result;
+    localStorage.setItem("authors",JSON.stringify(authors));
+}
+
+
+function deleteTableAuthor(i){
+    authors.splice(i,1);
+    authorsIndex =i;
+    displayDataInTable();
+    displayData();
+    localStorage.setItem("authors",JSON.stringify(authors));
+}
+
+function clearAllTableData(){
+    authors=[];
+    data.innerHTML="";
+    displayDataInTable();
+    displayData();
+    localStorage.setItem("authors",JSON.stringify(authors));
+}
+
+clearAll.addEventListener("click",()=>{
+    clearAllTableData();
+})
